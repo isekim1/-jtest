70bf431ad596e346a560c5d5b8b3154fb8c750f1
wkdeo12 <buronson@naver.com>
2018-11-20 12:16:36 +0900

    테스트 코드작성

---
+++ Sample/Sample.js
@@ -0,0 +1,26 @@
+ 
+//  let name = "ggg"
+//  let age = 0;
+//  console.log(age);
+
+let name = "name1"
+let len = name.length;
+console.log(len);
+
+let newName= name.replace ('jun','Sam');
+console.log(newName);
+
+let char = newName.charAt(2);
+console.log(char);
+
+console.log(newName.toUpperCase());
+console.log(newName.toLowerCase()); 
+
+var x = 0;
+x += 5;
+console.log(x);
+
+console.log('hello' + 'world');
+console.log ('1' + 2  +3 + 4);
+
+console.log(123 == '123');
